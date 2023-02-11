2a1421d64f9246bf301503fe9e72920b85b0251e
Andrei Khoroshunov <andrei.khoroshunov@gmail.com>
2023-02-12 00:18:45 +0300

    add Randome

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,5 +1,10 @@
-﻿// 3_1 programm
-int numberA = 3;
-int numberB = 25;
+﻿// 3_3 programm
+int numberA = new Random().Next(1, 10); // 1 2 3 4 ... 9
+Console.WriteLine("первое число: ");
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine("второе число: ");
+Console.WriteLine(numberB);
 int result = numberA + numberB;
+Console.WriteLine("Сумма: ");
 Console.Write(result);
