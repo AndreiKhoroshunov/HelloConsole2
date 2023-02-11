6466e1cd39fa3ca365b93fb97182b77e7f1aea58
Andrei Khoroshunov <andrei.khoroshunov@gmail.com>
2023-02-12 00:46:33 +0300

    add Example 005 Condition IF ELSE

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿// 5_2 IfElse 
+Console.WriteLine("Ваше имя?");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("ВАЙ МЭ, это же Маша!");
+}
+else
+{
+    Console.Write("привет, ");
+    Console.WriteLine(username);
+}
